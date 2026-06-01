b8848f8000dc112a02e474d2cd4668c1abd43fad
Keishi Asai <isheik808@gmail.com>
2026-06-01 06:16:02 -0700

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.cpp
@@ -0,0 +1,30 @@
+#include <cctype>
+
+class Solution {
+    public:
+    bool isPalindrome(string s) {
+        int r = s.length()-1;
+        int l = 0;
+
+        while(l<r) {
+            while(l<r && !alphaNum(s[l])) {
+                l++;
+            }
+            while(l<r && !alphaNum(s[r])) {
+                r--;
+            }
+
+            if(tolower(s[l])!=tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+    bool alphaNum(char c) {
+        return (c>='A' && c<='Z'||
+                c>='a' && c<='z'||
+                c>='0' && c<='9');
+    }
+};
