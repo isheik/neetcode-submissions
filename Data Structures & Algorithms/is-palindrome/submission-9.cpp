07bf2d97bfede2e2e626a3284486be0ac509828c
Keishi Asai <isheik808@gmail.com>
2026-06-01 05:52:53 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.cpp
@@ -0,0 +1,25 @@
+#include <cctype>
+
+class Solution {
+    public:
+    bool isPalindrome(string s) {
+        int r = s.length();
+        int l = 0;
+
+        while(l<=r) {
+            while(l<=r && !isalnum(s[l])) {
+                l++;
+            }
+            while(l<=r && !isalnum(s[r])) {
+                r--;
+            }
+
+            if(l<=r && tolower(s[l])!=tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
