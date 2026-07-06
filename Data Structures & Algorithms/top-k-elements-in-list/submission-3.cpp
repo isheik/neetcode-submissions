06215dedb64c3460219bf0fb0061199c01c6cf17
Keishi Asai <isheik808@gmail.com>
2026-07-06 06:47:37 -0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // create a max heap <number, that's appearance>
+        // get top element k times
+        auto cmp = [](const pair<int, int>& a, const pair<int,int>& b){
+            return a.second < b.second;
+        };
+        priority_queue<pair<int,int>, vector<pair<int, int>>, decltype(cmp)> heap(cmp);
+
+
+//        priority_queue<pair<int,int>> heap;
+
+
+
+        unordered_map<int, int> m;
+
+        for(int i=0; i<nums.size(); i++) {
+            m[nums[i]]++;
+        }
+
+        // int i=0;       while(i<nums.size()) {
+        //     int c = 0;
+            
+        //     for(int j=0; j<nums.size(); j++) {
+        //         if(nums[i] == nums[j]) {
+        //             c++;
+        //         }
+
+        //         heap.insert({i, c});
+
+        //         i++;
+        //     }
+        // }
+
+        for(const auto& entry : m) {
+//            heap.push({entry.second, entry.first});
+            heap.push({entry.first, entry.second});
+        }
+
+        vector<int> result;
+
+        for(int j=0; j<k; j++) {
+//            result.push_back(heap.top().second);
+            result.push_back(heap.top().first);
+            heap.pop();
+        }
+        
+
+        return result;
+    }
+};
