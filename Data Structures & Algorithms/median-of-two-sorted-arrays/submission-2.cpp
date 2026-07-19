957f88676b1aa5bac2305447b1e0d8ae15f5bf57
Keishi Asai <isheik808@gmail.com>
2026-07-19 06:35:39 -0700

    Add: median-of-two-sorted-arrays - submission-2

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-2.cpp
@@ -0,0 +1,68 @@
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         // concat?=> no, it takes n already
+//         // BS... what to find?
+//         vector<int> arr;
+
+//         for(int i=0; i<nums1.size(); i++) {
+//             arr.push_back(nums1[i]);
+//         }
+
+//         for(int i=0; i<nums2.size(); i++) {
+//             arr.push_back(nums2[i]);
+//         }
+
+//         sort(arr.begin(), arr.end());
+//         double median;
+//         if(arr.size()%2 == 1) {
+//             median = arr[arr.size()/2];
+//         } else {
+//             median = (arr[arr.size()/2-1] + arr[arr.size()/2])/2.0;
+//         }
+        
+//         return median;
+//     }
+// };
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>& A = nums1;
+        vector<int>& B = nums2;
+
+        int total = A.size() + B.size();
+        int half = (total+1)/2;
+
+        if(B.size() < A.size()) {
+            swap(A, B);
+        }
+
+        int l=0;
+        int r=A.size();
+
+        while(l<=r) {
+            int i = (l+r) / 2;
+            int j = half - i;
+
+            int Aleft = i > 0 ? A[i-1] : INT_MIN;
+            int Aright = i <A.size() ? A[i] : INT_MAX;
+            int Bleft = j>0? B[j-1] : INT_MIN;
+            int Bright = j<B.size() ?B[j] : INT_MAX;
+
+            if(Aleft <= Bright && Bleft <= Aright) {
+                if(total %2 != 0) {
+                    return max(Aleft, Bleft);
+                }
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
+            } else if(Aleft > Bright) {
+                r=i-1;
+            } else {
+                l=i+1;
+            }
+        }
+
+        return -1;
+
+    }
+};
