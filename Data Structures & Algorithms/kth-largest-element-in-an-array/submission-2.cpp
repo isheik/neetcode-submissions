99b9dfae704f39b12700fd72dfb11a674d05d0ef
Keishi Asai <isheik808@gmail.com>
2026-07-22 11:27:12 -0700

    Add: kth-largest-element-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-2.cpp
@@ -0,0 +1,16 @@
+// Time complexity O(nlogn)
+// space O(1)
+class Solution {
+    private:
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        auto cmp = [](const auto& a, const auto& b){
+            return a>b;
+        };
+
+        sort(nums.begin(), nums.end(), cmp);
+
+        return nums[k-1];
+    }
+
+};
